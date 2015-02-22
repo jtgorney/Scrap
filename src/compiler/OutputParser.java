3af6125c57c5b59a083b9dd1834d4e601c0e57a1
Matthew <Matthew@Matthew>
2015-02-21 21:26:42 -0500

    Fixed error in parsing logic of OutputParser.

--- src/compiler/OutputParser.java
+++ src/compiler/OutputParser.java
@@ -15,9 +15,9 @@ import java.util.Scanner;
  */
 public class OutputParser {
     
-    private final String status;
-    private String stdOutput;
-    private String stdError;
+    private String status = "";
+    private String stdOutput = "";
+    private String stdError = "";
     
     /**
      *
@@ -27,24 +27,21 @@ public class OutputParser {
         Scanner s = new Scanner(f);
         status = s.nextLine();
         String line = s.nextLine();
-        if (! line.equals(CodeProcessor.END_FILE)) {
+        if (line.equals(CodeProcessor.BEGIN_OUTPUT)) {
             line = s.nextLine();
-            if (line.equals(CodeProcessor.BEGIN_OUTPUT)) {
+            if (line.equals(CodeProcessor.BEGIN_STD_OUT)) {
                 line = s.nextLine();
-                if (line.equals(CodeProcessor.BEGIN_STD_OUT)) {
+                while (! line.equals(CodeProcessor.END_STD_OUT)) {
+                    stdOutput += line.substring(1) + "\n";
                     line = s.nextLine();
-                    while (! line.equals(CodeProcessor.END_STD_OUT)) {
-                        stdOutput += line.substring(1) + "\n";
-                        line = s.nextLine();
-                    }
                 }
+            }
+            line = s.nextLine();
+            if (line.equals(CodeProcessor.BEGIN_STD_ERR)) {
                 line = s.nextLine();
-                if (line.equals(CodeProcessor.BEGIN_STD_ERR)) {
+                while (! line.equals(CodeProcessor.END_STD_ERR)) {
+                    stdError += line.substring(1) + "\n";
                     line = s.nextLine();
-                    while (! line.equals(CodeProcessor.END_STD_ERR)) {
-                        stdError += line.substring(1) + "\n";
-                        line = s.nextLine();
-                    }
                 }
             }
         }
