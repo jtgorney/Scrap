85285e4da968d1aae21d6952ddf8ef2fc4ea439e
Matthew <Matthew@Matthew>
2015-03-28 15:54:13 -0400

    Updated license headers to match other files.

--- src/compiler/CompiledApplicationBuilder.java
+++ src/compiler/CompiledApplicationBuilder.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Matt Mossner
+ * Copyright (c) 2015 Jacob Gorney, Max Savard, Matt Mossner, Spencer Kokaly
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
